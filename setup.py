8e0c2db4773130c3b8c991d8cf0077cf046105d7
Goncalo-Soares12 <a042475@ipmaia.pt>
2025-05-28 14:53:53 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
-
 from setuptools import setup, find_packages
+from os import path
+
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='gestao-imc',
-    version='0.1.0',
-    packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'matplotlib',
-        'datetime'
-    ],
-    author='Gonçalo Soares, Ruben Abreu, Flávio Santos, Jessica Gonçalves', 
+    name='gestao_imc',
+    version='0.1.2',
+    url='https://github.com/Goncalo-Soares12/MADS-Grupo-4---Gestao-IMC-',
+    author='Gonçalo Soares, Ruben Abreu, Flávio Santos, Jessica Gonçalves',
     author_email='seu@email.com',
     description='Ferramenta para gestão de IMC com base em dados de pessoas.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/seu-usuario/gestao-imc',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-    ],
-    python_requires='>=3.6',
+    packages=find_packages(),
+    install_requires=['pandas', 'matplotlib', 'datetime'],
 )
